fbc6ed9ee0eceb2f33740ba1a4cf461702916f64
Andre <andre@aaindustries.de>
2026-05-27 17:23:26 +0200

    - updated to latest recommended settings & increased version number

---
+++ Name badge Watch App/ViewModels/MainViewModel.swift
@@ -0,0 +1,70 @@
+/// The view model for the main view
+final class MainViewModel: ObservableObject {
+    /// The color the user selected for the background
+    @Published var selectedBackground: String = UserDefaults.standard.selectedBackgroundColor ?? Color.black.description {
+        didSet {
+            UserDefaults.standard.selectedBackgroundColor = selectedBackground
+        }
+    }
+    
+    /// The color the user selected for the background
+    @Published var selectedForeground: String = UserDefaults.standard.selectedForegroundColor ?? Color.white.description {
+        didSet {
+            UserDefaults.standard.selectedForegroundColor = selectedForeground
+        }
+    }
+    
+    /// Indicator, if the selected background image should be used or a plain background color
+    @Published var useBackgroundImage: Bool = UserDefaults.standard.useBackgroundImage {
+        didSet {
+            UserDefaults.standard.useBackgroundImage = useBackgroundImage
+        }
+    }
+    
+    @Published var selectedItem: PhotosPickerItem? = nil {
+        didSet {
+            guard let selectedItem else { return }
+            Task {
+                do {
+                    // Prefer requesting Data for control over size/encoding
+                    if let data = try await selectedItem.loadTransferable(type: Data.self) {
+                        self.imageData = data
+                    }
+                } catch {
+                    // Handle error
+                    print("Error: \(error)")
+                }
+            }
+        }
+    }
+    
+    @Published var imageData: Data? = UserDefaults.standard.backgroundImageData {
+        didSet {
+            UserDefaults.standard.backgroundImageData = imageData
+        }
+    }
+    
+    @Published var backgroundImageRenderingMode: ImageRenderMode = UserDefaults.standard.backgroundImageRenderingMode {
+        didSet {
+            UserDefaults.standard.backgroundImageRenderingMode = backgroundImageRenderingMode
+        }
+    }
+    
+    @Published var backgroundImageOpacity: Double = UserDefaults.standard.backgroundImageOpacity {
+        didSet {
+            UserDefaults.standard.backgroundImageOpacity = backgroundImageOpacity
+        }
+    }
+    
+    /// The date which is shown on the badge
+    @Published var selectedDate: Date = .now
+    
+    
+    init() {
+        /// For now we will always update the current date when the app opens again
+        NotificationCenter.default.addObserver(forName: WKApplication.willEnterForegroundNotification, object: nil, queue: .main) { _ in
+            
+            self.selectedDate = .now
+        }
+    }
+}
